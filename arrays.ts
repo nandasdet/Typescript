0434d31b514dcc6d1d35f9432520c03318577916
nanda <baskara.sadananda@gmail.com>
2026-02-09 14:43:51 +0530

    updated arrays file

--- arrays.ts
+++ arrays.ts
@@ -4,3 +4,9 @@ let subjects:string[]=['lang1','lang2','maths','esv','vedicmaths'];
 for(let subject of subjects){
     console.log(subject)
 }
+
+let stuMarks=[10,20,30,40,20,10];
+
+for(let sm in stuMarks){
+    console.log(stuMarks[sm]);
+}
